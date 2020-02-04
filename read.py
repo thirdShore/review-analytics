452ed0e1b0cee4d4d1d572d78756f16707ddc092
Zhang Chen <18741306@qq.com>
2020-02-04 12:15:31 +0800

    calc avg lenth of reviews

--- read.py
+++ read.py
@@ -7,7 +7,11 @@ with open('reviews.txt','r') as f:
 		if count % 10000 == 0:
 			print(len(data))
 
-#print(len(data))
-print('\n', data[0])
-print('---------------------')
-print(data[1])
+print('档案读取完了，总共有',len(data),'条留言')
+#print('\n', data[0])
+#print('---------------------')
+#print(data[1])
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+print('留言平均长度为',sum_len / len(data))
